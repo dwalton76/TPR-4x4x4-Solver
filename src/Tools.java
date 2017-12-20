8ee8add4e95a02704a99e708aeb83c6b22620c8e
Daniel Walton <dwalton76@gmail.com>
2017-12-20 14:11:04 -0500

    Change tabs to whitespaces

--- src/Tools.java
+++ src/Tools.java
@@ -5,87 +5,87 @@ import java.util.Random;
 
 public class Tools {
 
-	private static void read(int[] arr, DataInput in) throws IOException {
-		for (int i=0, len=arr.length; i<len; i++) {
-			arr[i] = in.readInt();
-		}
-	}
+    private static void read(int[] arr, DataInput in) throws IOException {
+        for (int i=0, len=arr.length; i<len; i++) {
+            arr[i] = in.readInt();
+        }
+    }
 
-	private static void write(int[] arr, DataOutput out) throws IOException {
-		for (int i=0, len=arr.length; i<len; i++) {
-			out.writeInt(arr[i]);
-		}
-	}
+    private static void write(int[] arr, DataOutput out) throws IOException {
+        for (int i=0, len=arr.length; i<len; i++) {
+            out.writeInt(arr[i]);
+        }
+    }
 
-	private static void read(int[][] arr, DataInput in) throws IOException {
-		for (int i=0, leng=arr.length; i<leng; i++) {
-			for (int j=0, len=arr[i].length; j<len; j++) {
-				arr[i][j] = in.readInt();
-			}
-		}	
-	}
+    private static void read(int[][] arr, DataInput in) throws IOException {
+        for (int i=0, leng=arr.length; i<leng; i++) {
+            for (int j=0, len=arr[i].length; j<len; j++) {
+                arr[i][j] = in.readInt();
+            }
+        }    
+    }
 
-	private static void write(int[][] arr, DataOutput out) throws IOException {
-		for (int i=0, leng=arr.length; i<leng; i++) {
-			for (int j=0, len=arr[i].length; j<len; j++) {
-				out.writeInt(arr[i][j]);
-			}
-		}	
-	}
+    private static void write(int[][] arr, DataOutput out) throws IOException {
+        for (int i=0, leng=arr.length; i<leng; i++) {
+            for (int j=0, len=arr[i].length; j<len; j++) {
+                out.writeInt(arr[i][j]);
+            }
+        }    
+    }
 
-	static Random r = new Random();
+    static Random r = new Random();
 
-	public static String randomCube() {
-		return randomCube(r);
-	}
+    public static String randomCube() {
+        return randomCube(r);
+    }
 
-	public static String randomCube(Random r) {
-		FullCube c = new FullCube(r);
-		byte[] f = new byte[96];
-		c.toFacelet(f);
-		StringBuffer sb = new StringBuffer();
-		for (byte i: f) {
-			sb.append("URFDLB".charAt(i));
-		}
-		return sb.toString();
-	}
+    public static String randomCube(Random r) {
+        FullCube c = new FullCube(r);
+        byte[] f = new byte[96];
+        c.toFacelet(f);
+        StringBuffer sb = new StringBuffer();
+        for (byte i: f) {
+            sb.append("URFDLB".charAt(i));
+        }
+        return sb.toString();
+    }
 
-	public synchronized static void initFrom(DataInput in) throws IOException {
-		if (Search.inited) {
-			return;
-		}
+    public synchronized static void initFrom(DataInput in) throws IOException {
+        if (Search.inited) {
+            return;
+        }
 
-		System.out.println("Initialize Center1 Solver...");
+        System.out.println("Initialize Center1 Solver...");
 
-		Center1.initSym();
-		Center1.initSym2Raw();
-		read(Center1.ctsmv, in);
-		Center1.createPrun();
+        Center1.initSym();
+        Center1.initSym2Raw();
+        read(Center1.ctsmv, in);
+        Center1.createPrun();
 
-		System.out.println("Initialize Center2 Solver...");
+        System.out.println("Initialize Center2 Solver...");
 
-		Center2.init();
+        Center2.init();
 
-		System.out.println("Initialize Center3 Solver...");
+        System.out.println("Initialize Center3 Solver...");
 
-		Center3.init();
+        Center3.init();
 
-		System.out.println("Initialize Edge3 Solver...");
+        System.out.println("Initialize Edge3 Solver...");
 
-		Edge3.initMvrot();
-		Edge3.initRaw2Sym();
-		read(Edge3.eprun, in);
+        Edge3.initMvrot();
+        Edge3.initRaw2Sym();
+        read(Edge3.eprun, in);
 
-		System.out.println("OK");		
+        System.out.println("OK");        
 
-		Search.inited = true;
-	}
+        Search.inited = true;
+    }
 
-	public synchronized static void saveTo(DataOutput out) throws IOException {
-		if (!Search.inited) {
-			Search.init();
-		}
-		write(Center1.ctsmv, out);
-		write(Edge3.eprun, out);
-	}
+    public synchronized static void saveTo(DataOutput out) throws IOException {
+        if (!Search.inited) {
+            Search.init();
+        }
+        write(Center1.ctsmv, out);
+        write(Edge3.eprun, out);
+    }
 }
